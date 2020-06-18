c088bd8252545295f0b327e1f28240dcdeaf63ef
wildpomme <wildpomme@posteo.net>
2020-06-17 21:34:58 -0500

    Initial commit

---
+++ Change-Shipbreaker-ShiftTime.ps1
@@ -0,0 +1,52 @@
+param (
+    [int]$shiftMinutes = 15,
+    [bool]$restoreBackup = $false
+)
+
+[single]$shiftSeconds = $shiftMinutes * 60
+
+$shiftRelative = "\Shipbreaker_Data\sharedassets0.assets"
+$game = Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | 
+ForEach-Object { Get-ItemProperty $_.PsPath } | Where-Object {$_.DisplayName -eq "Hardspace: Shipbreaker"}
+
+$installLocation = $game.InstallLocation
+
+if ($restoreBackup -eq $true) {
+    if (Test-Path "$installLocation$shiftRelative.bak") {
+        Copy-Item -Path "$installLocation$shiftRelative.bak" -Destination "$installLocation$shiftRelative"
+        Remove-Item -Path "$installLocation$shiftRelative.bak"
+    }
+    else {
+        Write-Error "A backup cannot be restored because a backup does not exist."
+    }
+
+    exit
+}
+else {
+    if (!(Test-Path "$installLocation$shiftRelative.bak")) {
+        Copy-Item -Path "$installLocation$shiftRelative" -Destination "$installLocation$shiftRelative.bak"
+    }
+
+    $signal = "43-61-72-65-65-72-4D-6F-64-65-5F-4C-65-76-65-6C-41-73-73-65-74"
+    $offset1 = 0x412C4B8
+    $offset2 = 0x412C4CC
+    $bytes = [System.IO.File]::ReadAllBytes("$installLocation$shiftRelative")
+    $signalTest = [System.BitConverter]::ToString($bytes[$offset1..$offset2])
+
+    if ($signal -eq $signalTest) {
+        $shiftOffset = 0x412C508
+
+        $boots = [System.BitConverter]::GetBytes([single]$shiftSeconds)
+
+        for($i = 0; $i -lt 4; $i++) {
+            $bytes[$shiftOffset+$i] = $boots[$i]
+        }
+
+        [System.IO.File]::WriteAllBytes("$installLocation$shiftRelative", $bytes)
+    }
+    else {
+        Write-Error "The shift time could not be found. The game file must have been changed."
+    }
+
+    exit
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # shipbreaker-shift-mod
 This is a mod for the game Hardspace: Shipbreaker that changes the value of the shift timer.
+
+To use the mod, you must open Windows PowerShell and run the script.
+
+For example, to change the shift time from the default of 15 minutes to 30 minutes,
+you would run the Powershell script like this:
+
+    .\Change-Shipbreaker-ShiftTime.ps1 -ShiftMinutes 30
+
+When the time is changed, a backup of the original game file is made.
+If you would like to restore the backup, so that you are using the original game file,
+you would run the PowerShell script like this:
+
+    .\Change-Shipbreaker-ShiftTime.ps1 -RestoreBackup $true
